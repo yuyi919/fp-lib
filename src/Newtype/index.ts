3d8ca7ffb60aad0f016a9ab9c0709865820231de
yuyi919 <yuyi919606952@foxmail.com>
2023-09-07 19:44:43 +0800

    init

--- src/Newtype/index.ts
+++ src/Newtype/index.ts
@@ -1,7 +1,7 @@
 export * from "./newtype";
 
 declare module "fp-ts/HKT" {
-  type URItoKind<A> = Newtypes.Types<A>;
+  interface URItoKind<A> extends Newtypes.Types<A> {}
   interface URItoKind2<E, A> extends Newtypes.Types<A> {
     [Newtypes.URI]: E;
   }
